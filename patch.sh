24a62ac398282a75e7030a2653fed9a8e2c29b43
Matthew Wilding <mbwilding@gmail.com>
2025-05-09 11:46:53 +0800

    Release v0.0.4

--- patch.sh
+++ patch.sh
@@ -6,13 +6,11 @@ if [ ! -f Changes.md ]; then
     echo "Changes.md not found"
     exit 1
 fi
-
 VERSION=$(grep "^##" Changes.md | head -n 1 | sed -E 's/^##[[:space:]]+([0-9]+\.[0-9]+\.[0-9]+).*/\1/')
 if [ -z "$VERSION" ]; then
     echo "Version not found in Changes.md"
     exit 1
 fi
-
 TAG="v${VERSION}"
 
 # Workspace Cargo.toml
--- release.sh
+++ release.sh
@@ -4,6 +4,17 @@ set -e
 
 ./patch.sh
 
+if [ ! -f Changes.md ]; then
+    echo "Changes.md not found"
+    exit 1
+fi
+VERSION=$(grep "^##" Changes.md | head -n 1 | sed -E 's/^##[[:space:]]+([0-9]+\.[0-9]+\.[0-9]+).*/\1/')
+if [ -z "$VERSION" ]; then
+    echo "Version not found in Changes.md"
+    exit 1
+fi
+TAG="v${VERSION}"
+
 if git rev-parse "refs/tags/${TAG}" >/dev/null 2>&1; then
   echo "Error: Tag ${TAG} already exists." >&2
   exit 1
